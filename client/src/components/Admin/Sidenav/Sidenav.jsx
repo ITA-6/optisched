ad3c858e9727950876b97136dff505b118c07ee9
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-08-25 16:42:53 +0800

    added two navigation pages to admins

--- client/src/components/Admin/Sidenav/Sidenav.jsx
+++ client/src/components/Admin/Sidenav/Sidenav.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import {Outlet, Link } from "react-router-dom";
 
 import opti from "../../../assets/opti.png";
 import dash from "../../../assets/dash.svg";
@@ -7,31 +7,37 @@ import generate from "../../../assets/generate.png";
 
 const Sidenav = () => {
   return (
-    <nav className="absolute min-h-screen w-[8.125em] bg-green pt-8">
-      <div className="mb-8 flex justify-center">
-        <img src={opti} alt="OptiSched Logo" />
-      </div>
-      <ul className="flex flex-col items-center gap-4">
-        <li className="w-full bg-dark-green">
-          <Link to="#" className="flex flex-col items-center py-2">
-            <img src={dash} alt="Dashboard Icon" />
-            <span>Dashboard</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="#" className="flex flex-col items-center py-2">
-            <img src={user} alt="Users Icon" />
-            <span>Users</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="#" className="flex flex-col items-center py-2">
-            <img src={generate} alt="Dashboard Icon" />
-            <span className="text-center leading-5">Generate Schedule</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="absolute min-h-screen w-[8.125em] bg-green pt-8">
+        <div className="mb-8 flex justify-center">
+          <img src={opti} alt="OptiSched Logo" />
+        </div>
+        <ul className="flex flex-col items-center gap-4">
+          <li className="w-full bg-dark-green">
+            <Link to="dashboard" className="flex flex-col items-center py-2">
+              <img src={dash} alt="Dashboard Icon" />
+              <span>Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="user" className="flex flex-col items-center py-2">
+              <img src={user} alt="Users Icon" />
+              <span>Users</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="flex flex-col items-center py-2">
+              <img src={generate} alt="Dashboard Icon" />
+              <span className="text-center leading-5">Generate Schedule</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    
+    
+    </>
+   
   );
 };
 
