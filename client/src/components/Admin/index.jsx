7f23e0abff04f71066a38c9b686f371fbc13237d
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-08-28 14:36:41 +0800

    added new header and text can change dynamically

--- client/src/components/Admin/index.jsx
+++ client/src/components/Admin/index.jsx
@@ -1,11 +1,37 @@
+import React from 'react';
+import { useLocation} from 'react-router-dom';
+import Header from './Sidenav/Header';
+import Sidenav from './Sidenav';
 
-import Sidenav from "./Sidenav";
 const Admin = () => {
-  return (
-    <>
-      <Sidenav />
-    </>
-  );
+    const location = useLocation();
+
+    const getPageName = (path) => {
+        switch (path) {
+            case '/admin':
+            case '/admin/dashboard':
+                return 'Admin Dashboard';
+            case '/admin/user/manage':
+                return 'Manage Users';
+            case '/admin/management/professor':
+                return 'Manage Professors';
+            case '/admin/management/section':
+                return 'Manage Sections';
+            case '/admin/management/classroom':
+                return 'Manage Classrooms';
+            case '/admin/management/course':
+                return 'Manage Courses';
+            default:
+                return 'Admin Dashboard';
+        }
+    };
+
+    return (
+        <>
+            <Header pageName={getPageName(location.pathname)} />
+            <Sidenav />
+        </>
+    );
 };
 
 export default Admin;
