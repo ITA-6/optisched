87589b952b2cc8474545f56834f35228325fa319
AceSumaoy <sumaoyace78@gmail.com>
2024-08-28 12:14:15 +0800

    Sidenav Update

--- client/src/components/Admin/Sidenav/Sidenav.jsx
+++ client/src/components/Admin/Sidenav/Sidenav.jsx
@@ -1,86 +1,141 @@
-import {Outlet, Link } from "react-router-dom";
-
-import opti from "../../../assets/opti.png";
+import { Outlet, Link } from "react-router-dom";
+import optisched from "../../../assets/optisched.png";
+import pncHeader from "../../../assets/pncHeader.png";
 import dash from "../../../assets/dash.svg";
 import user from "../../../assets/user.png";
 import generate from "../../../assets/generate.png";
 import parameters from "../../../assets/parameters.png";
 import menu from "../../../assets/menu.png";
+import { useState } from "react";
 
 const Sidenav = () => {
-  const managementDropDown = () => {
-     const manage = document.querySelector(".manangement");
-     console.log(manage);
-  }
+  const [isManagementOpen, setIsManagementOpen] = useState(false);
+  const [isUserOpen, setIsUserOpen] = useState(false);
+
+  const toggleManagementDropdown = () => {
+    setIsManagementOpen(!isManagementOpen);
+  };
+
+  const toggleUserDropdown = () => {
+    setIsUserOpen(!isUserOpen);
+  };
+
   return (
     <>
-      <nav className="absolute min-h-screen w-[7em] bg-green pt-8">
-        <div className="mb-8 flex justify-center">
-          <img src={opti} alt="OptiSched Logo" />
+      <nav className="absolute min-h-screen w-[16em] bg-white pt-8 text-black">
+        <div className="flex justify-center">
+          <img src={optisched} alt="OptiSched Logo" className="h-16 w-auto" />
         </div>
-        <ul className="flex flex-col items-center gap-4">
-          <li className="w-full bg-dark-green">
-            <Link to="dashboard" className="flex flex-col items-center py-2">
-              <img src={dash} alt="Dashboard Icon" className="h-9 w-9" />
-              <span>Dashboard</span>
+        <div className="mb-8 mt-0 flex justify-center">
+          <img src={pncHeader} alt="Pnc Header" className="h-10 w-auto" />
+        </div>
+        <div className="mb-4 flex justify-center bg-grayish">
+          <h1 className="text-xl">Scheduling System</h1>
+        </div>
+        <ul className="flex flex-col gap-4 px-4">
+          <li className="w-full">
+            <Link
+              to="dashboard"
+              className="flex w-full items-center rounded-lg px-1 py-4 hover:bg-dark-green"
+            >
+              <img src={dash} alt="Dashboard Icon" className="h-10 w-10" />
+              <span className="text-md ml-4">Dashboard</span>
             </Link>
           </li>
-          <li>
-            <Link to="user" className="flex flex-col items-center py-2">
+          <li className="w-full">
+            <button
+              type="button"
+              onClick={toggleUserDropdown}
+              className="flex w-full items-center rounded-lg px-2 py-4 hover:bg-dark-green"
+            >
               <img src={user} alt="Users Icon" className="h-7 w-7" />
-              <span>Users</span>
+              <span className="text-md ml-6">Users</span>
+            </button>
+            {isUserOpen && (
+              <ul className="mt-2 flex w-full flex-col gap-2 rounded-lg bg-dark-green py-4">
+                <li className="w-full">
+                  <Link
+                    to="#"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 py-2"
+                  >
+                    <span className="ml-2 text-sm">Manage</span>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className="w-full">
+            <Link
+              to="Generate"
+              className="flex w-full items-center rounded-lg px-2 py-4 hover:bg-dark-green"
+            >
+              <img src={generate} alt="Generate Icon" className="h-8 w-8" />
+              <span className="text-md ml-5">Generate Schedule</span>
             </Link>
           </li>
-          <li>
-          <Link to="#" className="flex flex-col items-center">
-            <span className="text-center leading-5">Manage</span>
-          </Link>
-        </li>
-        <li>
-            <Link to="Generate" className="flex flex-col items-center py-2">
-              <img src={generate} alt="Dashboard Icon" className="h-8 w-8" />
-              <span className="text-center leading-5">Generate Schedule</span>
+          <li className="w-full">
+            <Link
+              to="#"
+              className="flex w-full items-center rounded-lg px-2 py-4 hover:bg-dark-green"
+            >
+              <img
+                src={parameters}
+                alt="Parameters Icon"
+                className="h-10 w-10"
+              />
+              <span className="text-md ml-4">Parameters</span>
             </Link>
           </li>
-          <li>
-          <Link to="#" className="flex flex-col items-center py-2">
-            <img src={parameters} alt="Parameters Icon" className="h-10 w-10" />
-            <span>Parameters</span>
-          </Link>
-        </li>
-        <li>
-          <button type="button"  onClick={managementDropDown} className="flex flex-col items-center py-2 management">
-            <img src={menu} alt="Menu Icon" className="h-9 w-9" />
-            <span className="text-center leading-5">Management Systems</span>
-          </button>
-        </li>
-        <li>
-          <Link to="Professor" className="flex flex-col items-center">
-            <span className="text-center leading-5">Professor</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="Section" className="flex flex-col items-center">
-            <span className="text-center leading-5">Section</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="Classroom" className="flex flex-col items-center">
-            <span className="text-center leading-5">Classroom</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="Course" className="flex flex-col items-center">
-            <span className="text-center leading-5">Course</span>
-          </Link>
-        </li>
-      </ul>
+          <li className="w-full">
+            <button
+              type="button"
+              onClick={toggleManagementDropdown}
+              className="flex w-full items-center rounded-lg px-2 py-4 hover:bg-dark-green"
+            >
+              <img src={menu} alt="Menu Icon" className="h-9 w-9" />
+              <span className="text-md ml-4">Management Systems</span>
+            </button>
+            {isManagementOpen && (
+              <ul className="mt-2 flex w-full flex-col gap-2 rounded-lg bg-dark-green py-2">
+                <li className="w-full">
+                  <Link
+                    to="Professor"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 py-2"
+                  >
+                    <span className="ml-4 text-sm">Professor</span>
+                  </Link>
+                </li>
+                <li className="w-full">
+                  <Link
+                    to="Section"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 py-2"
+                  >
+                    <span className="ml-4 text-sm">Section</span>
+                  </Link>
+                </li>
+                <li className="w-full">
+                  <Link
+                    to="Classroom"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 py-2"
+                  >
+                    <span className="ml-4 text-sm">Classroom</span>
+                  </Link>
+                </li>
+                <li className="w-full">
+                  <Link
+                    to="Course"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 py-2"
+                  >
+                    <span className="ml-4 text-sm">Course</span>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
       </nav>
       <Outlet />
-    
-    
     </>
-   
   );
 };
 
