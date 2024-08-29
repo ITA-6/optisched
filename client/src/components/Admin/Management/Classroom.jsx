128225fc22b86404ec4cd15bdcd936264b337d4f
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-08-29 23:20:39 +0800

    form modal update

--- client/src/components/Admin/Management/Classroom.jsx
+++ client/src/components/Admin/Management/Classroom.jsx
@@ -1,7 +1,12 @@
 import user from "../../../assets/user.png";
 import classroom from "../../../assets/classroom.png";
-
+import add from "../../../assets/add.png"
+import { useState } from "react";
 const Classroom = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
     <div className="h-screen w-screen bg-white-grayish">
       <div className="ml-[18rem] mr-[2rem] grid h-screen grid-cols-[2fr_1fr] grid-rows-[1fr_7fr_4fr] grid-areas-user-layout">
@@ -67,7 +72,72 @@ const Classroom = () => {
                 </tbody>
             </table>
         </div>
+        <div className="mt-5 flex items-start justify-end grid-in-button">
+          <button
+            className="mr-5 flex h-20 w-52 items-center justify-center space-x-2 rounded-3xl border-2 border-black bg-light-green text-white"
+            onClick={openModal}>
+            <img src={add} alt="" className="h-[30px] w-[30px]" />
+            <span>Add New Classroom</span>
+          </button>
+        </div>
       </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative w-3/4 rounded-lg bg-white shadow-lg">
+            <div className="flex h-1/5 items-center justify-center bg-green">
+              <img src={user} alt="" className="m-3 mr-4 h-[30px] w-[30px]" />
+              <h2 className="ml-2 text-3xl font-extrabold">Create New Section</h2>
+            </div>
+            <div className="p-5">
+              <form className="mt-5 space-y-6">
+                <div className="flex items-center">
+                    <div className=" flex flex-1">
+                      <label htmlFor="classroomId" className="font-medium text-lg">
+                         Classroom ID :
+                         </label>
+                      <p 
+                      id="classroomId"
+                      name="classroomId"
+                      className="font-medium text-lg inlin"
+                      >
+                        1
+                      </p>
+                    </div>
+                </div>
+                <div className="flex flex-col flex-1">
+                  <label htmlFor="buildingName" className="font-medium text-lg"> Building Name</label>
+                  <input type="text" placeholder="Building Name" className="border border-gray-300 p-2 rounded-md" />
+                </div>
+                <div className="flex flex-col flex-1">
+                  <label htmlFor="floorNumber" className="font-medium text-lg"> Floor Number</label>
+                  <input type="number" placeholder="Floor Number" className="border border-gray-300 p-2 rounded-md" />
+                </div>
+                <div className="flex gap-5 ">
+                  <div className="flex flex-col flex-1">
+                    <label htmlFor="roomNumber" className="font-medium text-lg">Room Number</label>
+                    <input type="number" id="roomNumber" name="roomNumber" placeholder="Room Number" className="border border-gray-300 p-2 rounded-md" />
+                  </div>
+                  <div className="flex flex-col flex-1">
+                    <label htmlFor="roomCapacity" className="font-medium text-lg">Room Capacity</label>
+                    <input type="number" id="roomCapacity" name="roomCapacity" placeholder="Room Number" className="border border-gray-300 p-2 rounded-md" />
+                  </div>
+                </div>
+                <div className="ml-10 mt-5 flex items-start justify-end grid-in-button">
+                  <button className="mr-5 flex h-10 w-40 items-center justify-center rounded-2xl border-2 border-black bg-green">
+                    <span>Confirm</span>
+                  </button>
+                </div>
+              </form>
+              <button
+                className="absolute right-2 top-2 rounded-full bg-red-500 p-2 text-white"
+                onClick={closeModal}
+              >
+                &times;
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/components/Admin/Management/Courses.jsx
+++ client/src/components/Admin/Management/Courses.jsx
@@ -1,7 +1,12 @@
 import user from "../../../assets/user.png";
 import course from "../../../assets/course.png";
-
+import add from "../../../assets/add.png"
+import { useState } from "react";
 const Course = () =>{
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
     return (
         <div className="h-screen w-screen bg-white-grayish">
             <div className="grid grid-areas-user-layout grid-cols-[2fr_1fr] grid-rows-[1fr_7fr_4fr] h-screen ml-[18rem]  mr-[2rem]">
@@ -66,7 +71,82 @@ const Course = () =>{
                         </tbody>
                     </table>
                 </div>
+                <div className="mt-5 flex items-start justify-end grid-in-button">
+                    <button
+                        className="mr-5 flex h-20 w-52 items-center justify-center space-x-2 rounded-3xl border-2 border-black bg-light-green text-white"
+                        onClick={openModal}>
+                        <img src={add} alt="" className="h-[30px] w-[30px]" />
+                        <span>Add New Course</span>
+                    </button>
+                </div>
             </div>
+            {isModalOpen && (
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="relative w-3/4 rounded-lg bg-white shadow-lg">
+                    <div className="flex h-1/5 items-center justify-center bg-green">
+                    <img src={user} alt="" className="m-3 mr-4 h-[30px] w-[30px]" />
+                    <h2 className="ml-2 text-3xl font-extrabold">Create New Course</h2>
+                    </div>
+                    <div className="p-5">
+                    <form className="mt-5 space-y-6">
+                        <div className="flex items-center">
+                            <div className=" flex flex-1">
+                            <label htmlFor="classroomId" className="font-medium text-lg">
+                                Course ID :
+                                </label>
+                            <p 
+                            id="courseId"
+                            name="courseId"
+                            className="font-medium text-lg inlin"
+                            >
+                                1
+                            </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col flex-1">
+                            <label htmlFor="courseTitle" className="font-medium text-lg"> Course Title</label>
+                            <input type="text" id="courseTitle" name="courseTitle" placeholder="Course Title" className="border border-gray-300 p-2 rounded-md" />
+                        </div>
+                        <div className="flex flex-col flex-1">
+                            <label htmlFor="courseCode" className="font-medium text-lg"> Course Code</label>
+                            <input type="text" name="courseCode" id="courseCode" placeholder="Course Code" className="border border-gray-300 p-2 rounded-md" />
+                        </div>
+                        <div className="flex gap-5 ">
+                            <div className="flex flex-col flex-1">
+                                <label htmlFor="department" className="font-medium text-lg">Department</label>
+                                <select name="department" id="department" className="border border-gray-300 rounded-md p-2">
+                                    <option value="CCS">CCS</option>
+                                    <option value="CCS">CCS</option>
+                                    <option value="CCS">CCS</option>
+                                    <option value="CCS">CCS</option>
+                                    <option value="CCS">CCS</option>
+                                </select>
+                            </div>
+                            <div className="flex flex-col flex-1">
+                                <label htmlFor="courseType" className="font-medium text-lg">Course Type</label>
+                                <select name="courseType" id="courseType" className="border border-gray-300 rounded-md p-2">
+                                    <option value="CCS">Lecture</option>
+                                    <option value="CCS">Laboratory</option>
+                                    <option value="Both">Laboratory && Lecture</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="ml-10 mt-5 flex items-start justify-end grid-in-button">
+                        <button className="mr-5 flex h-10 w-40 items-center justify-center rounded-2xl border-2 border-black bg-green">
+                            <span>Confirm</span>
+                        </button>
+                        </div>
+                    </form>
+                    <button
+                        className="absolute right-2 top-2 rounded-full bg-red-500 p-2 text-white"
+                        onClick={closeModal}
+                    >
+                        &times;
+                    </button>
+                    </div>
+                </div>
+            </div>
+        )}
         </div>
   );
 };
