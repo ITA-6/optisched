3adba5b4264210d8597e6445e755a3b1aff584ea
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-08-30 14:27:29 +0800

    initial commit, currently on progress

---
+++ client/src/components/Admin/Generate/generatedForms/Generated.jsx
@@ -0,0 +1,13 @@
+
+
+const Generated =  () => {
+    return (
+        <div className="h-screen w-screen">
+            <div className="flex">
+                    Generated Form
+            </div>
+        </div>
+    )
+}
+
+export default Generated
