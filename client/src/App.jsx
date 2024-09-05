3585095bb0d62dec48e6c384830fd8c02d011bff
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-05 18:20:36 +0800

    added new routes for building and department

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,8 @@ import Professor from "./components/Admin/Management/Professors";
 import Section from "./components/Admin/Management/Section";
 import Classroom from "./components/Admin/Management/Classroom";
 import Course from "./components/Admin/Management/Courses";
-
+import Building from "./components/Admin/Management/Building";
+import Department from "./components/Admin/Management/Department";
 const App = () => {
   return (
     <BrowserRouter>
@@ -35,6 +36,8 @@ const App = () => {
             <Route path="management/section" element={<Section />} />
             <Route path="management/classroom" element={<Classroom />} />
             <Route path="management/course" element={<Course />} />
+            <Route path="management/building" element={<Building />} />
+            <Route path="management/department" element={<Department />} />
         </Route>
       </Routes>
     </BrowserRouter>
