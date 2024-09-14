59387a0d6107e8f6f74de13d11c0262d2c53e49f
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-09-15 00:39:58 +0800

    add new api for counting management

--- server/account/urls.py
+++ server/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterApiView
+from account.views import RegisterApiView, CountApiView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("register/", RegisterApiView.as_view(), name="register"),
     path("login/", obtain_auth_token, name="login"),
+    path("count/", CountApiView.as_view(), name="count"),
 ]
