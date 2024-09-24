a386b3ed47fc2be32203fb4246cb0d933a02318d
AceSumaoy <sumaoyace78@gmail.com>
2024-09-24 22:36:55 +0800

    Update Sidenav.jsx

--- client/src/components/Admin/Sidenav/Sidenav.jsx
+++ client/src/components/Admin/Sidenav/Sidenav.jsx
@@ -109,13 +109,22 @@ const Sidenav = () => {
                     to="management/professor"
                     className="hover:bg-green-700 mt-2 flex w-full items-center rounded-lg px-4 py-2 text-white"
                   >
+                    <span className="ml-4 text-sm">Constraints</span>
+                  </Link>
+                </li>
+
+                <li className="w-full">
+                  <Link
+                    to="management/professor"
+                    className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 text-white"
+                  >
                     <span className="ml-4 text-sm">Professor</span>
                   </Link>
                 </li>
                 <li className="w-full">
                   <Link
                     to="management/section"
-                    className="hover:bg-green-700 mb-2 flex w-full items-center rounded-lg px-4 text-white"
+                    className="hover:bg-green-700 mb-2 flex w-full items-center rounded-lg px-4 py-1 text-white"
                   >
                     <span className="ml-4 text-sm">Section</span>
                   </Link>
