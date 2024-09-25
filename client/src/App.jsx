c205fdeedc7243c4dd51478ab43eabbdd87fe838
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-25 22:58:54 +0800

    refactored the import of modules for viewing and management, added new routes and paths for user and sub admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,36 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./components/Login";
 
+// admin Imports
 import Admin from "./components/Admin";
 import Dashboard from "./components/Admin/Dashboard/Dashboard";
 import User from "./components/Admin/User/User";
 import Generate from "./components/Admin/Generate/Generate";
 import Generated from "./components/Admin/Generate/generatedForms/Generated";
 import Curriculum from "./components/Admin/Curriculum/Curriculum";
-import Professor from "./components/Admin/Management/Professor";
-import Section from "./components/Admin/Management/Section";
-import Classroom from "./components/Admin/Management/Classroom";
-import Course from "./components/Admin/Management/Course";
-import Building from "./components/Admin/Management/Building";
-import Department from "./components/Admin/Management/Department";
-import Program from "./components/Admin/Management/Program";
+import ViewProfessor from "./components/Admin/ViewManagement/Professor";
+import ViewSection from "./components/Admin/ViewManagement/Section";
+import ViewClassroom from "./components/Admin/ViewManagement/Classroom";
+import ViewCourse from "./components/Admin/ViewManagement/Course";
+import ViewDepartment from "./components/Admin/ViewManagement/Department";
+import ViewProgram from "./components/Admin/ViewManagement/Program";
+import ViewBuilding from "./components/Admin/ViewManagement/Building";
+import Constraint from "./components/Admin/Constraint";
 
+
+// sub Admin Imports
+import SubAdmin from "./components/SubAdmin/index"
+import Buildings from "../src/components/SubAdmin/Management/Building/Building"
+import Professors from "./components/SubAdmin/Management/Professor";
+import Sections from "./components/SubAdmin/Management/Section";
+import Classrooms from "./components/SubAdmin/Management/Classroom";
+import Courses from "./components/SubAdmin/Management/Course";
+import Departments from "./components/SubAdmin/Management/Department";
+import Programs from "./components/SubAdmin/Management/Program";
+
+// User
+import Users from "./components/Users/"
+import GenerateUser from "./components/Users/Generate"
 const App = () => {
   return (
     <BrowserRouter>
@@ -33,13 +49,30 @@ const App = () => {
           <Route path="generate" element={<Generate />} />
           <Route path="generated/:name" element={<Generated />} />
           <Route path="curriculum" element={<Curriculum />} />
-          <Route path="management/professor" element={<Professor />} />
-          <Route path="management/section" element={<Section />} />
-          <Route path="management/classroom" element={<Classroom />} />
-          <Route path="management/course" element={<Course />} />
-          <Route path="management/building" element={<Building />} />
-          <Route path="management/department" element={<Department />} />
-          <Route path="management/program" element={<Program />} />
+          <Route path="view/professor" element={<ViewProfessor />} />
+          <Route path="view/section" element={<ViewSection />} />
+          <Route path="view/classroom" element={<ViewClassroom />} />
+          <Route path="view/course" element={<ViewCourse />} />
+          <Route path="view/department" element={<ViewDepartment />} />
+          <Route path="view/program" element={<ViewProgram />} />
+          <Route path="view/building" element={<ViewBuilding />} />
+          <Route path="view/constraint" element={<Constraint />} />
+        </Route>
+
+          {/* Sub Admin Routes */}
+        <Route path="sub-admin" element={<SubAdmin />}>
+          <Route index element={<Professors/>} />
+          <Route path="management/building" element={<Buildings/>} />
+          <Route path="management/professor" element={<Professors />} />
+          <Route path="management/section" element={<Sections />} />
+          <Route path="management/classroom" element={<Classrooms />} />
+          <Route path="management/course" element={<Courses />} />
+          <Route path="management/department" element={<Departments />} />
+          <Route path="management/program" element={<Programs />} />
+        </Route>
+
+        <Route path="User" element={<Users />} >
+          <Route path="generate" element={<GenerateUser />} />
         </Route>
       </Routes>
     </BrowserRouter>
