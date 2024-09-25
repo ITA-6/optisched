24e9b67fabaddc50430287ac0e7992c2158f952d
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-25 22:54:37 +0800

    added page for users view for generating schedule

---
+++ client/src/components/Users/Generate/Generate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateUser = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+            <div>Generate</div>
+    </div>
+  )
+}
+
+export default GenerateUser
---
+++ client/src/components/Users/Sidenav/Sidenav.jsx
@@ -0,0 +1,53 @@
+import { Outlet, Link } from "react-router-dom";
+import optisched from "../../../assets/optisched.png";
+import pncHeader from "../../../assets/pncHeader.png";
+import { useState } from "react";
+
+const Sidenav = () => {
+  const [isManagementOpen, setIsManagementOpen] = useState(false);
+  const [isUserOpen, setIsUserOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleManagementDropdown = () => {
+    setIsManagementOpen(!isManagementOpen);
+  };
+
+  const toggleUserDropdown = () => {
+    setIsUserOpen(!isUserOpen);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <nav
+        className={`z-3 absolute top-0 min-h-screen w-[16em] bg-white pt-8 text-black`}
+      >
+        <div className="flex justify-center">
+          <img src={optisched} alt="OptiSched Logo" className="h-16 w-auto" />
+        </div>
+        <div className="mb-8 mt-0 flex justify-center">
+          <img src={pncHeader} alt="Pnc Header" className="h-10 w-auto" />
+        </div>
+        <div className="mb-4 flex justify-center bg-grayish">
+          <h1 className="m-2 text-xl">Scheduling System</h1>
+        </div>
+        <ul className="flex flex-col gap-4 px-4">
+            <li className="w-full">
+              <Link
+                to="generate"
+                className="hover:bg-green-700 flex w-full items-center rounded-lg px-4 text-black"
+              >
+                <span className="ml-4 text-sm">Generate</span>
+              </Link>
+            </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Sidenav;
