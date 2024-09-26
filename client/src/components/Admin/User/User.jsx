7c1baaf42234bd7ce62bdf2386010b328e005614
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-26 21:17:54 +0800

    fixed code structure and make it easy to read, modal not done yet

--- client/src/components/Admin/User/User.jsx
+++ client/src/components/Admin/User/User.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import user from "../../../assets/user.png";
 import add from "../../../assets/add.png";
-
+import UserSearchField from "./Files/UserSearchField";
+import UserTable from "./Files/UserTable";
 const User = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -12,159 +13,10 @@ const User = () => {
     <div className="h-screen w-screen bg-white-grayish">
       <div className="ml-[20rem] mr-[2rem] grid h-screen grid-cols-[2fr_1fr] grid-rows-[1fr_6fr_6fr] grid-areas-user-layout">
         <div className="mr-5 grid grid-rows-[1fr_4fr] grid-areas-user-table-layout grid-in-userTable">
-          <div className="grid h-full grid-cols-[8fr_2fr_2fr] items-center justify-center gap-5 grid-areas-user-filter grid-in-div">
-            <input
-              type="text"
-              placeholder="search"
-              className="rounded-md border pl-7 grid-in-search"
-            />
-            <div className="text-center grid-in-list">
-              <select className="w-full">
-                <option value="">List: All users</option>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </select>
-            </div>
-          </div>
-          <table className="table-fixed bg-white grid-in-table">
-            <thead className="bg-green">
-              <tr>
-                <th scope="col">User ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Email Address</th>
-                <th scope="col">Phone Number</th>
-                <th scope="col">Department</th>
-                <th scope="col">Type of User</th>
-                <th scope="col">Status</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody className="mb-10 h-full overflow-auto">
-              <tr className="h-[30px] text-center">
-                <th scope="row">1</th>
-                <td>John Doe</td>
-                <td>Male</td>
-                <td>johndoe2922@gmail.com</td>
-                <td>21312323213</td>
-                <td>CCS</td>
-                <td>Dean</td>
-                <td>Active</td>
-                <td>
-                  <div className="flex items-center justify-center">
-                    <div className="ml-5 flex gap-2">
-                      <button className="-h5 w-16 bg-green text-white">
-                        {" "}
-                        Edit
-                      </button>
-                      <button className="-h5 w-16 bg-red-500 text-white">
-                        {" "}
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr className="h-[30px] text-center">
-                <th scope="row">2</th>
-                <td>Jane Smith</td>
-                <td>Female</td>
-                <td>janesmith1234@gmail.com</td>
-                <td>31231231231</td>
-                <td>BSeD</td>
-                <td>Professor</td>
-                <td>Inactive</td>
-                <td>
-                  <div className="flex items-center justify-center">
-                    <div className="ml-5 flex gap-2">
-                      <button className="-h5 w-16 bg-green text-white">
-                        {" "}
-                        Edit
-                      </button>
-                      <button className="-h5 w-16 bg-red-500 text-white">
-                        {" "}
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr className="h-[30px] text-center">
-                <th scope="row">3</th>
-                <td>Michael Johnson</td>
-                <td>Male</td>
-                <td>michaeljohnson5678@gmail.com</td>
-                <td>42342342342</td>
-                <td>CoE</td>
-                <td>Registrar</td>
-                <td>Active</td>
-                <td>
-                  <div className="flex items-center justify-center">
-                    <div className="ml-5 flex gap-2">
-                      <button className="-h5 w-16 bg-green text-white">
-                        {" "}
-                        Edit
-                      </button>
-                      <button className="-h5 w-16 bg-red-500 text-white">
-                        {" "}
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr className="h-[30px] text-center">
-                <th scope="row">4</th>
-                <td>Emily Davis</td>
-                <td>Female</td>
-                <td>emilydavis9012@gmail.com</td>
-                <td>53453453453</td>
-                <td>BSBA</td>
-                <td>Department Chair</td>
-                <td>Inactive</td>
-                <td>
-                  <div className="flex items-center justify-center">
-                    <div className="ml-5 flex gap-2">
-                      <button className="-h5 w-16 bg-green text-white">
-                        {" "}
-                        Edit
-                      </button>
-                      <button className="-h5 w-16 bg-red-500 text-white">
-                        {" "}
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr className="h-[30px] text-center">
-                <th scope="row">5</th>
-                <td>Daniel Brown</td>
-                <td>Male</td>
-                <td>danielbrown3456@gmail.com</td>
-                <td>64564564564</td>
-                <td>CCS</td>
-                <td>Dean</td>
-                <td>Active</td>
-                <td>
-                  <div className="flex items-center justify-center">
-                    <div className="ml-5 flex gap-2">
-                      <button className="-h5 w-16 bg-green text-white">
-                        {" "}
-                        Edit
-                      </button>
-                      <button className="-h5 w-16 bg-red-500 text-white">
-                        {" "}
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <UserSearchField />
+          <UserTable />
         </div>
+        {/* add User Table */}
         <div className="mt-5 flex items-start justify-end grid-in-button">
           <button
             className="mr-5 flex h-20 w-52 items-center justify-center space-x-2 rounded-3xl border-2 border-black bg-light-green text-white"
