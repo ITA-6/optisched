bf6eb6730579911e9c84981695114b2427571aae
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-28 23:30:08 +0800

    added page of Printing Generated Schedule of the User

---
+++ client/src/components/Users/MySchedule/Files/ScheduleTable.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const ScheduleTable = () => {
+  return (
+    <table className='table-fixed mr-4 text-lg'>
+      <thead className='bg-green text-white' >
+        <tr>
+          <th>Time</th>
+          <th>Monda</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+          <th>Sunday</th>
+        </tr>
+      </thead>
+      <tbody className='h-4/5 bg-white'>
+        <tr>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+          <th className='h-[20rem] w-[5rem]'>
+            Ganto lang muna design, wala pa available na library for timetable
+          </th>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default ScheduleTable
---
+++ client/src/components/Users/MySchedule/Schedule.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBarsProgress } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate} from "react-router-dom"
+
+const Schedule = () => {
+  return (
+    <div className="flex h-screen  w-full bg-white-grayish overflow-auto">
+      <div className="flex flex-col mt-[7rem] ml-[17rem] h-[200vh] w-screen text-black text-2xl">
+         <div className="flex mb-5">
+            <FontAwesomeIcon 
+              icon={faBarsProgress} 
+              size='xl'
+              className='p-2 bg-light-green rounded-xl'
+            />
+            <h1 className="font-bold text-lg text-center ml-2 mt-2">My Schedule</h1>
+         </div>
+         
+         {/* table */}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Schedule
