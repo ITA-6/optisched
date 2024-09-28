a0a8de46f2b7ea8648554419fb8db253938b54e7
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-09-28 23:30:28 +0800

    added page of Printing Generated Schedule of the User

---
+++ client/src/components/Users/PrintSchedule/PrintSchedule.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrintSchedule = () => {
+  return (
+   <div className="h-screen w-screen flex justify-center items-center">
+    <h1 className="text-2xl font-bold">
+        this page is for printing of the schedule reports per user
+    </h1>
+   </div>
+  )
+}
+
+export default PrintSchedule
