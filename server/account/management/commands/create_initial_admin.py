8b250028cfbb0b61d5c3f8f15f934155ade129e7
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-09 20:13:32 +0800

    Correct the birth_date

--- server/account/management/commands/create_initial_admin.py
+++ server/account/management/commands/create_initial_admin.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+from datetime import datetime
+
 
 class Command(BaseCommand):
     help = "Create an initial superuser"
+
+    birth_date = "2024-12-12"
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
@@ -12,7 +16,8 @@ class Command(BaseCommand):
                 username="0000000",
                 first_name="Fernando",
                 last_name="Pendon",
-                password="0000-00-00",
+                password=self.birth_date,
+                birth_date=datetime.strptime(self.birth_date, "%Y-%m-%d"),
                 email="admin@example.com",
             )
             self.stdout.write(self.style.SUCCESS("Successfully created initial admin"))
