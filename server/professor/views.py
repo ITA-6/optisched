d4a996746eb4245baa892b6b53272940c878026b
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-10 16:07:25 +0800

    Update professor module

--- server/professor/views.py
+++ server/professor/views.py
@@ -6,6 +6,7 @@ from rest_framework.exceptions import NotFound
 
 from professor.models import Professor
 from professor.serializers import ProfessorSerializer
+from account.models import CustomUser
 from account.serializers import AccountSerializer
 from department.models import Department
 
@@ -25,6 +26,13 @@ class ProfessorManager:
         user = user_serializer.save()
         return user
 
+    @staticmethod
+    def get_user_by_id(id):
+        try:
+            return CustomUser.objects.get(user_id=id)
+        except CustomUser.DoesNotExist:
+            return None
+
 
 class ProfessorAPIView(APIView):
     authentication_classes = []
@@ -33,118 +41,86 @@ class ProfessorAPIView(APIView):
     def get(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
         if pk:
-            try:
-                professor = Professor.objects.get(pk=pk)
-                serializer = ProfessorSerializer(professor)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except Professor.DoesNotExist:
-                return Response(
-                    {"error": "Department not found."},
-                    status=status.HTTP_404_NOT_FOUND,
-                )
+            return self.get_professor_detail(pk)
         else:
-            professors = Professor.objects.all()
-            serializer = ProfessorSerializer(professors, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return self.get_all_professors()
 
     def post(self, request, *args, **kwargs):
         # Ensure department exists
         department_id = request.data.get("department")
-        if not Department.objects.filter(id=department_id).exists():
-            raise NotFound("Department not found")
+        if not self.is_valid_department(department_id):
+            return Response(
+                {"error": "Department not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
-        professor_data = {
-            "prof_id": request.data.get("prof_id"),
-            "first_name": request.data.get("first_name"),
-            "last_name": request.data.get("last_name"),
-            "middle_name": request.data.get("middle_name"),
-            "birth_date": request.data.get("birth_date"),
-            "department": request.data.get("department"),
-            "email": request.data.get("email"),
-            "gender": request.data.get("gender"),
-            "employment_status": request.data.get("employment_status"),
-            "required_units": request.data.get("required_units"),
-            "current_units": request.data.get("current_units"),
-            "handled_schedule": [],
-        }
+        professor_data = self.build_professor_data(request.data)
 
+        # Initialize serializer with professor data
         professor_serializer = ProfessorSerializer(data=professor_data)
         professor_serializer.is_valid(raise_exception=True)
-        professor = professor_serializer.save()
 
-        user_data = {
-            "user_id": professor.prof_id,
-            "username": professor.prof_id,
-            "email": professor.email,
-            "password": professor.birth_date.strftime("%Y-%m-%d"),
-            "first_name": professor.first_name,
-            "last_name": professor.last_name,
-            "middle_name": professor.middle_name,
-            "birth_date": professor.birth_date.strftime("%Y-%m-%d"),
-            "department": professor.department.id,
-            "professor": professor.id,
-        }
+        # Check if user already exists
+        user_id = professor_data.get("prof_id")
+        user = ProfessorManager.get_user_by_id(
+            user_id
+        )  # Replace with your logic to get a user by email or any other field
 
-        ProfessorManager.create_account(user_data)
+        if user:
+            # If user exists, just link the professor to the existing account
+            professor = professor_serializer.save()
+            user.professor = professor
+            user.save()
 
-        data = {
-            "message": "Professor has been created.",
-            "data": professor_serializer.data,
-        }
+            data = {
+                "message": "Professor has been created and linked to an existing user account.",
+                "data": professor_serializer.data,
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            # If user doesn't exist, create a new one
+            professor = professor_serializer.save()
 
-        return Response(data, status=status.HTTP_201_CREATED)
+            # Build the user data and create a new account
+            user_data = self.build_user_data(professor)
+            ProfessorManager.create_account(user_data)
+
+            data = {
+                "message": "Professor has been created with a new user account.",
+                "data": professor_serializer.data,
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
 
     def put(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
-
         try:
             professor = Professor.objects.get(prof_id=pk)
-        except Department.DoesNotExist:
+        except Professor.DoesNotExist:
             return Response(
-                {"message": "Professor not found."}, status=status.HTTP_404_NOT_FOUND
+                {"error": "Professor not found"}, status=status.HTTP_404_NOT_FOUND
             )
 
-        professor_data = {
-            "prof_id": request.data.get("prof_id"),
-            "first_name": request.data.get("first_name"),
-            "last_name": request.data.get("last_name"),
-            "middle_name": request.data.get("middle_name"),
-            "birth_date": request.data.get("birth_date"),
-            "department": request.data.get("department"),
-            "email": request.data.get("email"),
-            "gender": request.data.get("gender"),
-            "employment_status": request.data.get("employment_status"),
-            "required_units": request.data.get("required_units"),
-            "current_units": request.data.get("current_units"),
-            "handled_schedule": [],
-        }
+        department_id = request.data.get("department")
+        if not self.is_valid_department(department_id):
+            return Response(
+                {"error": "Department not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
+        professor_data = self.build_professor_data(request.data)
         professor_serializer = ProfessorSerializer(professor, data=professor_data)
         professor_serializer.is_valid(raise_exception=True)
         professor = professor_serializer.save()
-
-        user_data = {
-            "username": professor.prof_id,
-            "email": professor.email,
-            "password": professor.birth_date.strftime("%Y-%m-%d"),
-            "professor": professor.id,
-        }
-
-        ProfessorManager.create_account(user_data)
 
         data = {
             "message": "Professor has been updated.",
             "data": professor_serializer.data,
         }
-
         return Response(data, status=status.HTTP_200_OK)
 
     def delete(self, request, *args, **kwargs):
         pk = kwargs.get("pk")
-        print(pk)
         try:
-            instance = Professor.objects.get(prof_id=pk)
-            instance.delete()
+            professor = Professor.objects.get(prof_id=pk)
+            professor.delete()
             return Response(
                 {"message": "Professor has been deleted successfully."},
                 status=status.HTTP_204_NO_CONTENT,
@@ -153,3 +129,52 @@ class ProfessorAPIView(APIView):
             return Response(
                 {"error": "Professor not found"}, status=status.HTTP_404_NOT_FOUND
             )
+
+    # Helper methods
+    def get_professor_detail(self, pk):
+        try:
+            professor = Professor.objects.get(pk=pk)
+            serializer = ProfessorSerializer(professor)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Professor.DoesNotExist:
+            return Response(
+                {"error": "Professor not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def get_all_professors(self):
+        professors = Professor.objects.all()
+        serializer = ProfessorSerializer(professors, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def is_valid_department(self, department_id):
+        return Department.objects.filter(id=department_id).exists()
+
+    def build_professor_data(self, data):
+        return {
+            "prof_id": data.get("prof_id"),
+            "first_name": data.get("first_name"),
+            "last_name": data.get("last_name"),
+            "middle_name": data.get("middle_name"),
+            "birth_date": data.get("birth_date"),
+            "department": data.get("department"),
+            "email": data.get("email"),
+            "gender": data.get("gender"),
+            "employment_status": data.get("employment_status"),
+            "required_units": data.get("required_units"),
+            "current_units": data.get("current_units"),
+            "handled_schedule": [],
+        }
+
+    def build_user_data(self, professor):
+        return {
+            "user_id": professor.prof_id,
+            "username": professor.prof_id,
+            "email": professor.email,
+            "password": professor.birth_date.strftime("%Y-%m-%d"),  # Default password
+            "first_name": professor.first_name,
+            "last_name": professor.last_name,
+            "middle_name": professor.middle_name,
+            "birth_date": professor.birth_date.strftime("%Y-%m-%d"),
+            "department": professor.department.id,
+            "professor": professor.id,
+        }
