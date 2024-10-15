394d80c61fcdcafa262bfcdb6b1efad86b15ba27
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-16 04:09:52 +0800

    Fixed the fetching of building name

--- client/src/components/Admin/ViewManagement/Classroom/Files/ClassroomRow.jsx
+++ client/src/components/Admin/ViewManagement/Classroom/Files/ClassroomRow.jsx
@@ -1,13 +1,15 @@
-const ClassroomRow = ({classroom}) => {
-    return(
-        <>
-            <tr key={classroom.id} className="h-16">
-                <td className="border-y-2 border-gray-200">{classroom.building}</td>
-                <td className="border-y-2 border-gray-200">{classroom.number}</td>
-                <td className="border-y-2 border-gray-200">{classroom.floor}</td>
-            </tr>
-        </>
-    )
-}
+const ClassroomRow = ({ classroom }) => {
+  return (
+    <>
+      <tr key={classroom.id} className="h-16">
+        <td className="border-y-2 border-gray-200">
+          {classroom.building_name}
+        </td>
+        <td className="border-y-2 border-gray-200">{classroom.number}</td>
+        <td className="border-y-2 border-gray-200">{classroom.floor}</td>
+      </tr>
+    </>
+  );
+};
 
 export default ClassroomRow;
