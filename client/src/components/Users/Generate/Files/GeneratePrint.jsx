5d981d405c558a8add627d5ba86999a150e8ac93
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-10-17 22:48:15 +0800

    fixed low quaility of generated pdf of the schedule

--- client/src/components/Users/Generate/Files/GeneratePrint.jsx
+++ client/src/components/Users/Generate/Files/GeneratePrint.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPrint } from '@fortawesome/free-solid-svg-icons';
-
 import  pncHeader from "../../../../assets/pncHeader.png"
-
+import html2pdf from "html2pdf.js"
 
 const GeneratePrint = () => {
   const printPage = () => {
     // Dynamically create the content that you want to print
     const contentToPrint = `
-      <div class="flex flex-col justify-center px-14">
+      <div class="flex flex-col justify-center px-7 text-center">
         <div class="flex flex-col items-center mt-5">
           <p class="text-sm">Republic of the Philippines</p>
           <img src="${pncHeader}" alt="PNC Header" style="width:23rem;" />
@@ -37,93 +36,113 @@ const GeneratePrint = () => {
         </div>
 
         <div class="flex flex-col mt-4 mb-4">
-          <p class="font-bold text-sm">
+          <p class="font-bold text-sm text-start">
             Enlisted Schedules
           </p>
 
           <table class="mt-2 table-auto">
-            <thead class="text-xs">
-              <tr class="h-[1.5rem] text-xs text-black border border-collapse border-black">
-                <th class="border border-black">Course Code</th>
-                <th class="border border-black">Course Description</th>
-                <th class="border border-black w-[50px]">Lec Units</th>
-                <th class="border border-black w-[50px]">Lab Units</th>
-                <th class="border border-black">Day</th>
-                <th class="border border-black">Time</th>
-                <th class="border border-black">Room</th>
-                <th class="border border-black">Section</th>
+            <thead class="text-[12px]">
+              <tr class="h-[2.5rem] text-center -pt-[20px] text-black border-[0.5px] border-collapse border-black">
+                <th class="border-[0.5px] text-xs border-black">Course Code</th>
+                <th class="border-[0.5px] text-xs border-black">Course Description</th>
+                <th class="border-[0.5px] text-xs border-black w-[50px]">Lec Units</th>
+                <th class="border-[0.5px] text-xs border-black w-[50px]">Lab Units</th>
+                <th class="border-[0.5px] text-xs border-black">Day</th>
+                <th class="border-[0.5px] text-xs border-black w-[130px]">Time</th>
+                <th class="border-[0.5px] text-xs border-black">Room</th>
+                <th class="border-[0.5px] text-xs border-black">Section</th>
               </tr>
             </thead>
               <tbody class="text-xs font-medium">
                 <tr class="border border-black border-collapse">
-                  <th class="h-[1.5rem] border border-black font-medium">RIZ101</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Life and Works of Rizal</th>
-                  <th class="h-[1.5rem] border border-black font-medium">3</th>
-                  <th class="h-[1.5rem] border border-black font-medium">0</th>
-                  <th class="h-[1.5rem] border border-black font-medium">M/WF</th>
-                  <th class="h-[1.5rem] border border-black font-medium">9:00-10:00 AM/ 9:00-10:00 AM</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Room 101</th>
-                  <th class="h-[1.5rem] border border-black font-medium">A1</th>
+                  <th class="  border border-black font-medium">RIZ101</th>
+                  <th class="text-start px-2  border border-black font-medium">Life and Works of Rizal</th>
+                  <th class="  border-[0.5px] border-black font-medium">3</th>
+                  <th class="  border-[0.5px] border-black font-medium">0</th>
+                  <th class="  border-[0.5px] border-black font-medium px-2">M/WF</th>
+                  <th class="  border-[0.5px] border-black font-medium px-1">9:00-10:00 AM/ 9:00-10:00 AM</th>
+                  <th class="  border-[0.5px] border-black font-medium">Room 101</th>
+                  <th class="  border-[0.5px] border-black font-medium px-5">A1</th>
                 </tr>
                 <tr class="border border-black border-collapse">
-                  <th class="h-[1.5rem] border border-black font-medium">CS101</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Introduction to Computer Science</th>
-                  <th class="h-[1.5rem] border border-black font-medium">3</th>
-                  <th class="h-[1.5rem] border border-black font-medium">1</th>
-                  <th class="h-[1.5rem] border border-black font-medium">T/Th</th>
-                  <th class="h-[1.5rem] border border-black font-medium">10:00-11:30 AM/ 9:00-10:00 AM</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Room 202</th>
-                  <th class="h-[1.5rem] border border-black font-medium">B2</th>
+                  <th class="  border-[0.5px] border-black font-medium">CS101</th>
+                  <th class="text-start px-2  border-[0.5px] border-black font-medium">Introduction to Computer Science</th>
+                  <th class="  border-[0.5px] border-black font-medium">3</th>
+                  <th class="  border-[0.5px] border-black font-medium">1</th>
+                  <th class="  border-[0.5px] border-black font-medium">T/Th</th>
+                  <th class="  border-[0.5px] border-black font-medium text-center px-1">10:00-11:30 AM/ 9:00-10:00 AM</th>
+                  <th class="  border-[0.5px] border-black font-medium px-5">Room 202</th>
+                  <th class="  border-[0.5px] border-black font-medium">B2</th>
                 </tr>
                 <tr class="border border-black border-collapse">
-                  <th class="h-[1.5rem] border border-black font-medium">MATH201</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Calculus I</th>
-                  <th class="h-[1.5rem] border border-black font-medium">4</th>
-                  <th class="h-[1.5rem] border border-black font-medium">0</th>
-                  <th class="h-[1.5rem] border border-black font-medium">M/WF</th>
-                  <th class="h-[1.5rem] border border-black font-medium">1:00-2:30 PM / 10:00-11:30 AM</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Room 105</th>
-                  <th class="h-[1.5rem] border border-black font-medium">C3</th>
+                  <th class="  border-[0.5px] border-black font-medium">MATH201</th>
+                  <th class="text-start px-2  border-[0.5px] border-black font-medium">Calculus I</th>
+                  <th class="  border-[0.5px] border-black font-medium">4</th>
+                  <th class="  border-[0.5px] border-black font-medium">0</th>
+                  <th class="  border-[0.5px] border-black font-medium">M/WF</th>
+                  <th class="  border-[0.5px] border-black font-medium px-1">1:00-2:30 PM / 10:00-11:30 AM</th>
+                  <th class="  border-[0.5px] border-black font-medium">Room 105</th>
+                  <th class="  border-[0.5px] border-black font-medium">C3</th>
                 </tr>
-                <tr class="border border-black border-collapse">
-                  <th class="h-[1.5rem] border border-black font-medium">PHY301</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Physics I</th>
-                  <th class="h-[1.5rem] border border-black font-medium">3</th>
-                  <th class="h-[1.5rem] border border-black font-medium">1</th>
-                  <th class="h-[1.5rem] border border-black font-medium">T/Th</th>
-                  <th class="h-[1.5rem] border border-black font-medium">3:00-4:30 PM</th>
-                  <th class="h-[1.5rem] border border-black font-medium">Room 303</th>
-                  <th class="h-[1.5rem] border border-black font-medium">D4</th>
+                <tr class="border-[0.5px] border-black border-collapse">
+                  <th class=" border-[0.5px] border-black font-medium">PHY301</th>
+                  <th class="text-start px-2  border-[0.5px] border-black font-medium">Physics I</th>
+                  <th class="  border-[0.5px] border-black font-medium">3</th>
+                  <th class="  border-[0.5px] border-black font-medium">1</th>
+                  <th class="  border-[0.5px] border-black font-medium">T/Th</th>
+                  <th class="  border-[0.5px] border-black font-medium px-1">3:00-4:30 PM</th>
+                  <th class="  border-[0.5px] border-black font-medium">Room 303</th>
+                  <th class="  border-[0.5px] border-black font-medium">D4</th>
                 </tr>
-                <tr class="border border-black border-collapse">
-                  <th class="h-[1.5rem] border border-black font-medium">ENG102</th>
-                  <th class="h-[1.5rem] border border-black font-medium">English Communication Skills</th>
-                  <th class="h-[1.5rem] border border-black font-medium">3</th>
-                  <th class="h-[1.5rem] border border-black font-medium">0</th>
-                  <th class="h-[1.5rem] border border-black font-medium">MWF</th>
-                  <th class="h-[1.5rem] border border-black font-medium">2:00-3:00 PM / 1:00-2:30 PM </th>
-                  <th class="h-[1.5rem] border border-black font-medium">Room 204</th>
-                  <th class="h-[1.5rem] border border-black font-medium">E5</th>
+                <tr class="border-[0.5px] border-black border-collapse">
+                  <th class="  border-[0.5px] border-black font-medium">ENG102</th>
+                  <th class="text-start px-2  border-[0.5px] border-black font-medium">English Communication Skills</th>
+                  <th class="  border-[0.5px] border-black font-medium">3</th>
+                  <th class="  border-[0.5px] border-black font-medium">0</th>
+                  <th class="  border-[0.5px] border-black font-medium">MWF</th>
+                  <th class="  border-[0.5px] border-black font-medium px-1">2:00-3:00 PM / 1:00-2:30 PM </th>
+                  <th class="  border-[0.5px] border-black font-medium">Room 204</th>
+                  <th class="  border-[0.5px] border-black font-medium">E5</th>
                 </tr>
               </tbody>
           </table>
+          <div class="flex justify-center items center text-xs mt-2 gap-5">
+              <p class="">
+                Total Units : <span class="ml2"> 18</span>
+              </p>
+              <p class=""> 
+                Lecture : <span class="ml-2"> 16 </span>
+              </p>
+              <p class="">
+                 Laboratory : <span class="ml-2"> 2</span>
+              </p>
+          </div>
         </div>
       </div>
     `;
 
+    const options = {
+      margin:       0, // Set margins (in mm)
+      filename:     'generated-pdf.pdf', // Name of the output PDF file
+      image:        { type: 'jpeg', quality: 1.0 }, // Set image quality to 100%
+      html2canvas:  { scale: 2 }, // Scale the canvas for higher quality
+      jsPDF:        { unit: 'mm', format: 'letter', orientation: 'portrait' } // A4 format
+    };
+    
     // Create a new element to contain the dynamically created content
     const element = document.createElement('div');
     element.innerHTML = contentToPrint;
-
+    
     // Use html2pdf to convert the dynamically generated content to PDF
     html2pdf()
       .from(element)
+      .set(options) // Apply the defined options
       .toPdf()
       .get('pdf')
       .then((pdf) => {
         const pdfBlob = pdf.output('blob');
         const pdfUrl = URL.createObjectURL(pdfBlob);
-
+    
         // Open the customized PDF in a new tab
         const newWindow = window.open(pdfUrl);
         if (newWindow) {
--- client/src/components/Users/Generate/Files/GenerateTable.jsx
+++ client/src/components/Users/Generate/Files/GenerateTable.jsx
@@ -16,55 +16,55 @@ const GenerateTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr class="border border-black border-collapse">
-          <th class="h-[1.5rem] border border-black font-medium">RIZ101</th>
-          <th class="h-[1.5rem] border border-black font-medium">Life and Works of Rizal</th>
-          <th class="h-[1.5rem] border border-black font-medium">3</th>
-          <th class="h-[1.5rem] border border-black font-medium">0</th>
-          <th class="h-[1.5rem] border border-black font-medium">M/WF</th>
-          <th class="h-[1.5rem] border border-black font-medium">9:00-10:00 AM</th>
-          <th class="h-[1.5rem] border border-black font-medium">Room 101</th>
-          <th class="h-[1.5rem] border border-black font-medium">A1</th>
+        <tr className="border border-black border-collapse">
+          <th className="h-[1.5rem] border border-black font-medium">RIZ101</th>
+          <th className="h-[1.5rem] border border-black font-medium">Life and Works of Rizal</th>
+          <th className="h-[1.5rem] border border-black font-medium">3</th>
+          <th className="h-[1.5rem] border border-black font-medium">0</th>
+          <th className="h-[1.5rem] border border-black font-medium">M/WF</th>
+          <th className="h-[1.5rem] border border-black font-medium">9:00-10:00 AM</th>
+          <th className="h-[1.5rem] border border-black font-medium">Room 101</th>
+          <th className="h-[1.5rem] border border-black font-medium">A1</th>
         </tr>
-        <tr class="border border-black border-collapse">
-          <th class="h-[1.5rem] border border-black font-medium">CS101</th>
-          <th class="h-[1.5rem] border border-black font-medium">Introduction to Computer Science</th>
-          <th class="h-[1.5rem] border border-black font-medium">3</th>
-          <th class="h-[1.5rem] border border-black font-medium">1</th>
-          <th class="h-[1.5rem] border border-black font-medium">T/Th</th>
-          <th class="h-[1.5rem] border border-black font-medium">10:00-11:30 AM</th>
-          <th class="h-[1.5rem] border border-black font-medium">Room 202</th>
-          <th class="h-[1.5rem] border border-black font-medium">B2</th>
+        <tr className="border border-black border-collapse">
+          <th className="h-[1.5rem] border border-black font-medium">CS101</th>
+          <th className="h-[1.5rem] border border-black font-medium">Introduction to Computer Science</th>
+          <th className="h-[1.5rem] border border-black font-medium">3</th>
+          <th className="h-[1.5rem] border border-black font-medium">1</th>
+          <th className="h-[1.5rem] border border-black font-medium">T/Th</th>
+          <th className="h-[1.5rem] border border-black font-medium">10:00-11:30 AM</th>
+          <th className="h-[1.5rem] border border-black font-medium">Room 202</th>
+          <th className="h-[1.5rem] border border-black font-medium">B2</th>
         </tr>
-        <tr class="border border-black border-collapse">
-          <th class="h-[1.5rem] border border-black font-medium">MATH201</th>
-          <th class="h-[1.5rem] border border-black font-medium">Calculus I</th>
-          <th class="h-[1.5rem] border border-black font-medium">4</th>
-          <th class="h-[1.5rem] border border-black font-medium">0</th>
-          <th class="h-[1.5rem] border border-black font-medium">M/WF</th>
-          <th class="h-[1.5rem] border border-black font-medium">1:00-2:30 PM</th>
-          <th class="h-[1.5rem] border border-black font-medium">Room 105</th>
-          <th class="h-[1.5rem] border border-black font-medium">C3</th>
+        <tr className="border border-black border-collapse">
+          <th className="h-[1.5rem] border border-black font-medium">MATH201</th>
+          <th className="h-[1.5rem] border border-black font-medium">Calculus I</th>
+          <th className="h-[1.5rem] border border-black font-medium">4</th>
+          <th className="h-[1.5rem] border border-black font-medium">0</th>
+          <th className="h-[1.5rem] border border-black font-medium">M/WF</th>
+          <th className="h-[1.5rem] border border-black font-medium">1:00-2:30 PM</th>
+          <th className="h-[1.5rem] border border-black font-medium">Room 105</th>
+          <th className="h-[1.5rem] border border-black font-medium">C3</th>
         </tr>
-        <tr class="border border-black border-collapse">
-          <th class="h-[1.5rem] border border-black font-medium">PHY301</th>
-          <th class="h-[1.5rem] border border-black font-medium">Physics I</th>
-          <th class="h-[1.5rem] border border-black font-medium">3</th>
-          <th class="h-[1.5rem] border border-black font-medium">1</th>
-          <th class="h-[1.5rem] border border-black font-medium">T/Th</th>
-          <th class="h-[1.5rem] border border-black font-medium">3:00-4:30 PM</th>
-          <th class="h-[1.5rem] border border-black font-medium">Room 303</th>
-          <th class="h-[1.5rem] border border-black font-medium">D4</th>
+        <tr className="border border-black border-collapse">
+          <th className="h-[1.5rem] border border-black font-medium">PHY301</th>
+          <th className="h-[1.5rem] border border-black font-medium">Physics I</th>
+          <th className="h-[1.5rem] border border-black font-medium">3</th>
+          <th className="h-[1.5rem] border border-black font-medium">1</th>
+          <th className="h-[1.5rem] border border-black font-medium">T/Th</th>
+          <th className="h-[1.5rem] border border-black font-medium">3:00-4:30 PM</th>
+          <th className="h-[1.5rem] border border-black font-medium">Room 303</th>
+          <th className="h-[1.5rem] border border-black font-medium">D4</th>
         </tr>
-        <tr class="border border-black border-collapse">
-          <th class="h-[1.5rem] border border-black font-medium">ENG102</th>
-          <th class="h-[1.5rem] border border-black font-medium">English Communication Skills</th>
-          <th class="h-[1.5rem] border border-black font-medium">3</th>
-          <th class="h-[1.5rem] border border-black font-medium">0</th>
-          <th class="h-[1.5rem] border border-black font-medium">M/WF</th>
-          <th class="h-[1.5rem] border border-black font-medium">2:00-3:00 PM</th>
-          <th class="h-[1.5rem] border border-black font-medium">Room 204</th>
-          <th class="h-[1.5rem] border border-black font-medium">E5</th>
+        <tr className="border border-black border-collapse">
+          <th className="h-[1.5rem] border border-black font-medium">ENG102</th>
+          <th className="h-[1.5rem] border border-black font-medium">English Communication Skills</th>
+          <th className="h-[1.5rem] border border-black font-medium">3</th>
+          <th className="h-[1.5rem] border border-black font-medium">0</th>
+          <th className="h-[1.5rem] border border-black font-medium">M/WF</th>
+          <th className="h-[1.5rem] border border-black font-medium">2:00-3:00 PM</th>
+          <th className="h-[1.5rem] border border-black font-medium">Room 204</th>
+          <th className="h-[1.5rem] border border-black font-medium">E5</th>
         </tr>
       </tbody>
     </table>
