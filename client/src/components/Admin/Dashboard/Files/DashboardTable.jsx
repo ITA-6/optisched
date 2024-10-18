cf4de5d9de0d3cbf7784e6add47a7339bab1869b
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-10-18 17:41:50 +0800

    fixed table not full width

--- client/src/components/Admin/Dashboard/Files/DashboardTable.jsx
+++ client/src/components/Admin/Dashboard/Files/DashboardTable.jsx
@@ -18,35 +18,37 @@ const DashboardTable = () => {
   }, []);
 
   return (
-    <table className="mb-16 table-fixed bg-white grid-in-table">
-      <thead className="border-separate border border-gray-100 bg-green text-xs text-white">
-        <tr>
-          <th scope="col" className="border border-white p-2">
-            Name
-          </th>
-          <th scope="col" className="border border-white">
-            Login Time
-          </th>
-          <th scope="col" className="border border-white">
-            IP Address
-          </th>
-          <th scope="col" className="border border-white">
-            Browser
-          </th>
-          <th scope="col" className="border border-white">
-            OS
-          </th>
-          <th scope="col" className="border border-white">
-            Device
-          </th>
-        </tr>
-      </thead>
-      <tbody className="text-sm">
-        {datas?.map((data, index) => (
-          <DashboardRow key={index} data={data} />
-        ))}
-      </tbody>
-    </table>
+    <div className="grid h-full grid-cols-[1fr_8fr_1fr] text-center grid-areas-table grid-in-history">
+      <table className="mb-16 table-fixed bg-white grid-in-table">
+        <thead className="border-separate border border-gray-100 bg-green text-xs text-white">
+          <tr>
+            <th scope="col" className="border border-white p-2">
+              Name
+            </th>
+            <th scope="col" className="border border-white">
+              Login Time
+            </th>
+            <th scope="col" className="border border-white">
+              IP Address
+            </th>
+            <th scope="col" className="border border-white">
+              Browser
+            </th>
+            <th scope="col" className="border border-white">
+              OS
+            </th>
+            <th scope="col" className="border border-white">
+              Device
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-sm">
+          {datas?.map((data, index) => (
+            <DashboardRow key={index} data={data} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
