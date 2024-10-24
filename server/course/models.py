37693ab587aece7d9ce88dcaf9fb18e3ba2096ad
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-24 23:37:34 +0800

    Update the course

--- server/course/models.py
+++ server/course/models.py
@@ -9,14 +9,22 @@ class Course(models.Model):
     )
 
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=50)
     category = models.CharField(max_length=25, choices=COURSE_CATEGORY)
     department = models.ForeignKey(
         "department.Department", on_delete=models.CASCADE, null=True, blank=True
     )
+    pre_requisites = models.ManyToManyField(
+        "self", blank=True, symmetrical=False, related_name="course_pre_requisites"
+    )
+    co_requisites = models.ManyToManyField(
+        "self", blank=True, symmetrical=False, related_name="course_co_requisites"
+    )
     sections = models.ManyToManyField("section.Section", blank=True)
-    total_units = models.IntegerField(default=0)
-    total_hours = models.IntegerField(default=0)
+    lecture_unit = models.IntegerField(default=0)
+    lab_unit = models.IntegerField(default=0)
+    lecture_hours = models.IntegerField(default=0)
+    lab_hours = models.IntegerField(default=0)
     need_masteral = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
