dd87ddaddb3da6e7aab068eb26715a7d29dab5af
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-24 23:41:06 +0800

    update the data structure

---
+++ server/data/curriculum/coe/bscpe.py
@@ -0,0 +1,9 @@
+BSCPE_CURRICULUM = [
+    {
+        "program": 9,
+        "department": 2,
+        "semester": "FIRST_SEMESTER",
+        "year_level": 1,
+        "courses": [{"course": "1"}],
+    }
+]
