c028b1d6891f3ee8043a8ab178383b55b370341b
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-26 18:48:28 +0800

    Update the request of data

--- server/curriculum/management/commands/create_initial_curriculum.py
+++ server/curriculum/management/commands/create_initial_curriculum.py
@@ -4,59 +4,76 @@ from program.models import Program
 from course.models import Course
 from curriculum.models import Curriculum
 
-# Define your curriculum data here or import from a separate file if needed
 from data.curriculum.ccs.bsit import BSIT_CURRICULUM
+from data.curriculum.ccs.bscs import BSCS_CURRICULUM
+from data.curriculum.cbaa.bsa import BSA_CURRICULUM
+from data.curriculum.chas.bsn import BSN_CURRICULUM
+from data.curriculum.coe.bscpe import BSCPE_CURRICULUM
+from data.curriculum.coe.bsece import BSECE_CURRICULUM
+from data.curriculum.coe.bsie import BSIE_CURRICULUM
 
 
 class Command(BaseCommand):
-    help = "Create curriculum for each department and program"
+    help = "Create or update curriculum for each department and program"
 
     def handle(self, *args, **kwargs):
-        for curriculum_data in BSIT_CURRICULUM:
-            try:
-                # Retrieve the program and department from the curriculum data
-                department = Department.objects.filter(
-                    id=curriculum_data["department"]
-                ).first()
-                program = Program.objects.filter(id=curriculum_data["program"]).first()
+        curriculum_data_groups = {
+            "ccs": BSIT_CURRICULUM + BSCS_CURRICULUM,
+            "cbaa": BSA_CURRICULUM,
+            "chas": BSN_CURRICULUM,
+            "coe": BSCPE_CURRICULUM + BSECE_CURRICULUM + BSIE_CURRICULUM,
+            "coed": [],
+            "bsp": [],
+        }
 
-                if not department:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            f"Department with ID {curriculum_data['department']} not found. Skipping..."
-                        )
-                    )
-                    continue
+        for group_name, curriculums in curriculum_data_groups.items():
+            for curriculum_data in curriculums:
+                self.process_curriculum(curriculum_data)
 
-                if not program:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            f"Program with ID {curriculum_data['program']} not found. Skipping..."
-                        )
-                    )
-                    continue
-
-                # Create or update curriculum for the semester and year level
-                self.create_curriculum(
-                    department,
-                    program,
-                    curriculum_data["year_level"],
-                    curriculum_data["semester"],
-                    curriculum_data["courses"],
-                )
-
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Curriculum created/updated for department {department.acronym}, program {program.acronym}"
-                    )
-                )
-
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(f"Error occurred: {str(e)}"))
-
-    def create_curriculum(self, department, program, year_level, semester, courses):
+    def process_curriculum(self, curriculum_data):
         try:
-            # Create or get the curriculum record
+            department = Department.objects.filter(
+                id=curriculum_data["department"]
+            ).first()
+            program = Program.objects.filter(id=curriculum_data["program"]).first()
+
+            if not department:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"Department with ID {curriculum_data['department']} not found. Skipping..."
+                    )
+                )
+                return
+
+            if not program:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"Program with ID {curriculum_data['program']} not found. Skipping..."
+                    )
+                )
+                return
+
+            self.create_or_update_curriculum(
+                department,
+                program,
+                curriculum_data["year_level"],
+                curriculum_data["semester"],
+                curriculum_data["courses"],
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Curriculum created/updated for department {department.acronym}, program {program.acronym}"
+                )
+            )
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error occurred: {str(e)}"))
+
+    def create_or_update_curriculum(
+        self, department, program, year_level, semester, courses
+    ):
+        try:
             curriculum, created = Curriculum.objects.get_or_create(
                 department=department,
                 program=program,
@@ -64,38 +81,31 @@ class Command(BaseCommand):
                 semester=semester,
             )
 
-            # Clear the courses if the curriculum already exists (optional)
             if not created:
                 curriculum.courses.clear()
 
-            # Add courses to the curriculum by course code
             for course_data in courses:
-                try:
-                    course = Course.objects.get(
-                        code=course_data["course"]
-                    )  # Get the course by code
-                    curriculum.courses.add(course)
-                except Course.DoesNotExist:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            f"Course with code {course_data['course']} does not exist. Skipping..."
-                        )
-                    )
+                self.add_course_to_curriculum(curriculum, course_data)
 
             curriculum.save()
 
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Created curriculum for {year_level} year, {semester}"
-                    )
+            message = "Created" if created else "Updated existing"
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"{message} curriculum for {year_level} year, {semester}"
                 )
-            else:
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"Updated existing curriculum for {year_level} year, {semester}"
-                    )
-                )
+            )
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
+
+    def add_course_to_curriculum(self, curriculum, course_data):
+        try:
+            course = Course.objects.get(code=course_data["course"])
+            curriculum.courses.add(course)
+        except Course.DoesNotExist:
+            self.stdout.write(
+                self.style.ERROR(
+                    f"Course with code {course_data['course']} does not exist. Skipping..."
+                )
+            )
--- server/curriculum/views.py
+++ server/curriculum/views.py
@@ -13,15 +13,15 @@ class CurriculumAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         # Get query parameters
-        department_id = self.request.query_params.get("department_id")
+        program_id = self.request.query_params.get("program_id")
         year_level = self.request.query_params.get("year_level")
         semester = self.request.query_params.get("semester")
 
-        # Start by fetching all Curriculum objects and filtering by department
+        # Start by fetching all Curriculum objects and filtering by program
         queryset = Curriculum.objects.prefetch_related("courses").all()
 
-        if department_id:
-            queryset = queryset.filter(department_id=department_id)
+        if program_id:
+            queryset = queryset.filter(program=program_id)
 
         if year_level:
             queryset = queryset.filter(year_level=year_level)
@@ -29,10 +29,10 @@ class CurriculumAPIView(APIView):
         if semester:
             queryset = queryset.filter(semester=semester)
 
-        # If no department was provided or found, return a 400 bad request
+        # If no program was provided or found, return a 400 bad request
         if not queryset.exists():
             return Response(
-                {"error": "No curriculum found for the specified department."},
+                {"error": "No curriculum found for the specified program."},
                 status=status.HTTP_404_NOT_FOUND,
             )
 
