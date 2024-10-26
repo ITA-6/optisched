d90019acec8e87e6c91481d5bbdca84a94e0cd5f
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-10-26 18:48:53 +0800

    Update curriculum

---
+++ server/data/curriculum/bsp/bsp.py
@@ -0,0 +1,93 @@
+PSY_CURRICULUM = [
+    # First Year First Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "FIRST_SEMESTER",
+        "year_level": 1,
+        "courses": [
+            {"course": "PSM101"},  # Introduction to Psychology
+            {"course": "PSM102"},  # Psychological Statistics
+            {"course": "PSM103"},  # Developmental Psychology
+            {"course": "PSM104"},  # Theories of Personality
+        ],
+    },
+    # First Year Second Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "SECOND_SEMESTER",
+        "year_level": 1,
+        "courses": [
+            {"course": "PSM105"},  # Cognitive Psychology
+            {"course": "PSE101"},  # Professional Ethics in Psychology
+            {"course": "PSM106"},  # Abnormal Psychology
+            {"course": "PSM107"},  # Experimental Psychology
+        ],
+    },
+    # Second Year First Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "FIRST_SEMESTER",
+        "year_level": 2,
+        "courses": [
+            {"course": "PSM108"},  # Psychological Assessment
+            {"course": "PSM109"},  # Sikolohiyang Filipino
+            {"course": "PSM110"},  # Industrial/Organizational Psychology
+            {"course": "PSM111"},  # Field Methods in Psychology
+        ],
+    },
+    # Second Year Second Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "SECOND_SEMESTER",
+        "year_level": 2,
+        "courses": [
+            {"course": "PSE102"},  # Introduction to Clinical Psychology
+            {"course": "PSM112"},  # Physiological Psychology
+            {"course": "PSE103"},  # Introduction to Counseling
+            {"course": "PSM113"},  # Research in Psychology 1
+        ],
+    },
+    # Third Year First Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "FIRST_SEMESTER",
+        "year_level": 3,
+        "courses": [
+            {"course": "PSM114"},  # Social Psychology
+            {"course": "PSE104"},  # Strategic Human Resource Management
+            {"course": "PSM115"},  # Research in Psychology 2
+            {
+                "course": "PSE105"
+            },  # Competency Appraisal 1 (Abnormal Psychology and Theories of Personality)
+        ],
+    },
+    # Third Year Second Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "SECOND_SEMESTER",
+        "year_level": 3,
+        "courses": [
+            {"course": "PSE106"},  # Entrepreneurial Psychology
+            {"course": "PSE107"},  # Practicum in Psychology 1
+            {
+                "course": "PSE108"
+            },  # Competency Appraisal 2 (Psychological Assessment and Industrial Psychology)
+        ],
+    },
+    # Fourth Year First Semester
+    {
+        "program": 2,
+        "department": 6,  # BSP Department
+        "semester": "FIRST_SEMESTER",
+        "year_level": 4,
+        "courses": [
+            {"course": "PSE109"},  # Practicum in Psychology 2
+        ],
+    },
+]
--- server/data/curriculum/ccs/bscs.py
+++ server/data/curriculum/ccs/bscs.py
@@ -1,4 +1,4 @@
-CURRICULUM = [
+BSCS_CURRICULUM = [
     # First Year First Semester
     {
         "program": 3,
@@ -6,9 +6,9 @@ CURRICULUM = [
         "semester": "FIRST_SEMESTER",
         "year_level": 1,
         "courses": [
-            {"course": "CCS101"},   # Introduction to Computing
-            {"course": "CCS102"},   # Computer Programming 1
-        ]
+            {"course": "CCS101"},  # Introduction to Computing
+            {"course": "CCS102"},  # Computer Programming 1
+        ],
     },
     # First Year Second Semester
     {
@@ -17,22 +17,22 @@ CURRICULUM = [
         "semester": "SECOND_SEMESTER",
         "year_level": 1,
         "courses": [
-            {"course": "CCS114"},   # Web Technologies
-            {"course": "CCS103"},   # Computer Programming 2
-            {"course": "CCS105"},   # Human-Computer Interaction 1
-        ]
+            {"course": "CCS114"},  # Web Technologies
+            {"course": "CCS103"},  # Computer Programming 2
+            {"course": "CCS105"},  # Human-Computer Interaction 1
+        ],
     },
-        # Second Year First Semester
+    # Second Year First Semester
     {
         "program": 3,
         "department": 1,  # Department
         "semester": "FIRST_SEMESTER",
         "year_level": 2,
         "courses": [
-            {"course": "CCS104"},   # Discrete Structures 1
-            {"course": "CCS107"},   # Data Structures and Algorithms
-            {"course": "CSP101"},   # Differential Calculus with Analytical Geometry
-        ]
+            {"course": "CCS104"},  # Discrete Structures 1
+            {"course": "CCS107"},  # Data Structures and Algorithms
+            {"course": "CSP101"},  # Differential Calculus with Analytical Geometry
+        ],
     },
     # Second Year Second Semester
     {
@@ -41,27 +41,29 @@ CURRICULUM = [
         "semester": "SECOND_SEMESTER",
         "year_level": 2,
         "courses": [
-            {"course": "CSP102"},   # Discrete Structures 2
-            {"course": "CCS108"},   # Object-Oriented Programming
-            {"course": "CSP104"},   # Integral Calculus with Differential Equations
-            {"course": "CCS113"},   # Information Assurance and Security
-            {"course": "CSP107"},   # Computer Organization and Assembly Language Programming
-        ]
+            {"course": "CSP102"},  # Discrete Structures 2
+            {"course": "CCS108"},  # Object-Oriented Programming
+            {"course": "CSP104"},  # Integral Calculus with Differential Equations
+            {"course": "CCS113"},  # Information Assurance and Security
+            {
+                "course": "CSP107"
+            },  # Computer Organization and Assembly Language Programming
+        ],
     },
-        # Third Year First Semester
+    # Third Year First Semester
     {
         "program": 3,
         "department": 1,  # Department
         "semester": "FIRST_SEMESTER",
         "year_level": 3,
         "courses": [
-            {"course": "CCS109"},   # System Analysis and Design
-            {"course": "CCS112"},   # Applications Development and Emerging Technologies
-            {"course": "CSP108"},   # Programming Languages
-            {"course": "CCS106"},   # Social Issues and Professional Practice
-            {"course": "CSP105"},   # Algorithms and Complexity
+            {"course": "CCS109"},  # System Analysis and Design
+            {"course": "CCS112"},  # Applications Development and Emerging Technologies
+            {"course": "CSP108"},  # Programming Languages
+            {"course": "CCS106"},  # Social Issues and Professional Practice
+            {"course": "CSP105"},  # Algorithms and Complexity
             {"course": "CS Elective 1"},  # Computer Science Elective 1
-        ]
+        ],
     },
     # Third Year Second Semester
     {
@@ -70,28 +72,28 @@ CURRICULUM = [
         "semester": "SECOND_SEMESTER",
         "year_level": 3,
         "courses": [
-            {"course": "CSP109"},   # Software Engineering 1
-            {"course": "CSP111"},   # Thesis 1
-            {"course": "TEC101"},   # Technopreneurship
-            {"course": "CSP106"},   # Automata Theory and Formal Languages
+            {"course": "CSP109"},  # Software Engineering 1
+            {"course": "CSP111"},  # Thesis 1
+            {"course": "TEC101"},  # Technopreneurship
+            {"course": "CSP106"},  # Automata Theory and Formal Languages
             {"course": "CS Elective 2"},  # Computer Science Elective 2
-            {"course": "CSP112"},   # Operating Systems
-        ]
+            {"course": "CSP112"},  # Operating Systems
+        ],
     },
-        # Fourth Year First Semester
+    # Fourth Year First Semester
     {
         "program": 3,
         "department": 1,  # Department
         "semester": "FIRST_SEMESTER",
         "year_level": 4,
         "courses": [
-            {"course": "CSP110"},   # Numerical Analysis
-            {"course": "CSP113"},   # Software Engineering 2
-            {"course": "CSP114"},   # Thesis 2
-            {"course": "CCS111"},   # Networking and Communication 1
-            {"course": "CPS101"},   # Probability and Statistics
+            {"course": "CSP110"},  # Numerical Analysis
+            {"course": "CSP113"},  # Software Engineering 2
+            {"course": "CSP114"},  # Thesis 2
+            {"course": "CCS111"},  # Networking and Communication 1
+            {"course": "CPS101"},  # Probability and Statistics
             {"course": "CS Elective 3"},  # Computer Science Elective 3
-        ]
+        ],
     },
     # Fourth Year Second Semester
     {
@@ -100,7 +102,7 @@ CURRICULUM = [
         "semester": "SECOND_SEMESTER",
         "year_level": 4,
         "courses": [
-            {"course": "CSP115"},   # CS Practicum (200 hours)
-        ]
-    }
+            {"course": "CSP115"},  # CS Practicum (200 hours)
+        ],
+    },
 ]
