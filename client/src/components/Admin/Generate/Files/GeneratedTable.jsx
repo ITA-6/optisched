e1a62d1a56c6c52daa9c4b930e1bea62bbad8a49
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-10-30 04:07:01 +0800

    added hover effect on the program_name table

--- client/src/components/Admin/Generate/Files/GeneratedTable.jsx
+++ client/src/components/Admin/Generate/Files/GeneratedTable.jsx
@@ -12,7 +12,7 @@ const GeneratedTable = ({filteredPrograms, viewProgram}) => {
         {filteredPrograms.length > 0 ? (
           filteredPrograms.map((program, index) => (
             <tr key={index} className="h-20 cursor-pointer">
-              <td onClick={() => viewProgram(program.name)}>
+              <td className='hover:bg-gray-100' onClick={() => viewProgram(program.name)}>
                 {program.name}
               </td>
             </tr>
