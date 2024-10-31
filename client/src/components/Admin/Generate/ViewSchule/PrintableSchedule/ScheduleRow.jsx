e4d69d94c68f7426d015577ba8a2cecdedece137
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-10-31 18:57:49 +0800

    fixed table border

--- client/src/components/Admin/Generate/ViewSchule/PrintableSchedule/ScheduleRow.jsx
+++ client/src/components/Admin/Generate/ViewSchule/PrintableSchedule/ScheduleRow.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const ScheduleRow = ({subject, sectionTable}) => {
   return (
-    <tr className="text-center text-xs border-collapse border-t border-l border-black">
-      <td className='text-[0.5rem] border-t border-l border-black'>{subject.professor_name ? subject.professor_name : "TBA"}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>{subject.course_code}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>{subject.course_description}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>{subject.lecture_units}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>{subject.lab_units}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>{subject.time}</td>
-      <td className='text-[0.5rem]  border-t border-l border-black'>TBA</td>
-      <td className='text-[0.5rem]  border border-black'>{sectionTable.year_level}{sectionTable.section_label}</td>
+    <tr className="text-center text-xs border-black">
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.professor_name ? subject.professor_name : "TBA"}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.course_code}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.course_description}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.lecture_units}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.lab_units}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>{subject.time}</td>
+      <td className='text-[0.5rem]  border-b border-l border-black'>TBA</td>
+      <td className='text-[0.5rem]  border-b border-l border-r border-black'>{sectionTable.year_level}{sectionTable.section_label}</td>
     </tr>
   )
 }
--- client/src/components/Admin/Generate/ViewSchule/PrintableSchedule/ScheduleTable.jsx
+++ client/src/components/Admin/Generate/ViewSchule/PrintableSchedule/ScheduleTable.jsx
@@ -5,14 +5,14 @@ const ScheduleTable = ({sectionArray}) => {
     <table className=" table-auto w-full">
         <thead>
           <tr className='border-collapse border-black border-t border-l'>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Professor Name</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Course Code</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Course Description</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Lec</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Lab</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Time</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l'>Room</th>
-              <th className=' text-[0.5rem] text-black border-black border-t border-l border-r'>Section</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Professor Name</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Course Code</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Course Description</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Lec</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Lab</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Time</th>
+              <th className=' text-[0.5rem] text-black border-black border-t border-l border-b'>Room</th>
+              <th className=' text-[0.5rem] text-black border-black border'>Section</th>
           </tr>
         </thead>
         <tbody className='border-t border-l border-gray-300 text-xs '>
