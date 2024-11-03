0aa745abb50b8c6f9a01b60eb513decad9ae2115
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-11-03 19:54:44 +0800

    Update timeout

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
  */
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
-  timeout: 5000,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
   },
