c439fef667228c43b6e80375f30a5af684df87f6
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-11-04 10:37:55 +0800

    Update the timeout

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 /**
  * Axios instance configured to interact with the API backend.
  * - Base URL: `http://127.0.0.1:8000/api/`
- * - Timeout: 5000ms
+ * - Timeout: 30000ms
  * - Content-Type: `application/json`
  *
  * @constant {AxiosInstance} api
