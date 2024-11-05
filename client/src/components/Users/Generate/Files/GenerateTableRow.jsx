927c4a5fa3ee8ad84131a5fd1e730b120834cb46
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-11-05 18:10:04 +0800

    dynamic table and fixed page layout overflowing

---
+++ client/src/components/Users/Generate/Files/GenerateTableRow.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GenerateTableRow = () => {
+  return (
+    <tr>
+        <td>schedule</td>
+        <td>schedule</td>
+        <td>schedule</td>
+        <td>schedule</td>
+        <td>schedule</td>
+        <td>schedule</td>
+        <td>schedule</td>
+    </tr>
+  )
+}
+
+export default GenerateTableRow
