c0fa47419b99f872739a35f10286d340b69e6e83
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-11-06 19:25:17 +0800

    added responsive for search

--- client/src/components/Admin/Dashboard/Files/DashboardSearchField.jsx
+++ client/src/components/Admin/Dashboard/Files/DashboardSearchField.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const DashboardSearchField = () => {
   return (
-    <div className="grid grid-cols-[5fr_0.5fr_1fr] items-center grid-areas-text-user grid-in-text">
-        <p className="mt-5 text-xl grid-in-userText xm:text-base">User Login History</p>
-        <div className="grid-in-search mt-5 xm:hidden">
+    <div className="grid sm:grid-cols-[3fr_5fr_0fr] md:grid-cols-[4fr_1.5fr_1fr]  grid-cols-[5fr_0.5fr_1fr] items-center grid-areas-text-user grid-in-text">
+        <p className="mt-5 text-xl grid-in-userText xm:text-xs sm:text-sm md:text-base lg:text-lg">User Login History</p>
+        <div className="flex grid-in-search mt-5 xm:hidden sm:justify-end">
             <input
                 type="text"
                 name="search"
                 id="search"
                 placeholder="Search"
-                className="rounded-md border pl-7"
+                className=" w-2/3 rounded-md border pl-7 xm:p-1 xm:text-xs sm:p-1 sm:pl-4 sm:text-xs md:text-sm md:p-1 md:pl-7"
             />
         </div>
-        <div className="ml-7 text-center grid-in-list mt-5 xm:hidden">
+        <div className="ml-7 text-center grid-in-list mt-5 xm:hidden sm:hidden md:block">
             <select className="w-full rounded-md border border-gray-200">
                 <option value="">List: All users</option>
                 <option value="option1">Option 1</option>
