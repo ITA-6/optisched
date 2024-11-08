4765f074df2fce08f02f0ef33ce514e843ff8e93
Mourey James Pantanosas <moureyjamesp@gmail.com>
2024-11-08 10:56:17 +0800

    Stop tracking generated_schedules.json

--- client/src/components/Users/Generate/Generate.jsx
+++ client/src/components/Users/Generate/Generate.jsx
@@ -22,7 +22,6 @@ const Generated = () => {
     fetchData();
   }, []);
 
-
   return (
     <div className="min-h-screen bg-gray-50">
       <div
@@ -31,25 +30,27 @@ const Generated = () => {
         <GeneratePrint />
         <div className="bg-white px-5 pt-5 grid-in-table">
           <div className="grid grid-cols-[1fr-1fr] grid-rows-[0.5fr_0.5fr_0.5fr_8fr_1fr] grid-areas-generated-table xm:mt-5">
-          {data?.map((sched) => (
-            <>
-              <h1 className="text-base font-bold grid-in-profName sm:text-sm md:text-base xm:text-xs">
-                Professor name : {sched.professor.first_name} {sched.professor.last_name}
-              </h1>
-              <h1 className="text-base font-bold grid-in-departmentName sm:text-sm md:text-base xm:text-xs">
-                Department : {sched.professor.department_name}
-              </h1>
-              <div className="flex items-center justify-between py-2 grid-in-units">
-                <h1 className="text-xl font-medium sm:text-sm lg:text-lg xm:text-xs">
-                  Total Units : 18
+            {data?.map((sched) => (
+              <>
+                <h1 className="text-base font-bold grid-in-profName sm:text-sm md:text-base xm:text-xs">
+                  Professor name : {sched.professor.first_name}{" "}
+                  {sched.professor.last_name}
                 </h1>
-              </div>
-            </>
-          ))}
-            <div className="grid-in-table overflow-auto h-[500px]"> {/* Set horizontal overflow here */}
-                <GenerateTable data={data} />
+                <h1 className="text-base font-bold grid-in-departmentName sm:text-sm md:text-base xm:text-xs">
+                  Department : {sched.professor.department_name}
+                </h1>
+                <div className="flex items-center justify-between py-2 grid-in-units">
+                  <h1 className="text-xl font-medium sm:text-sm lg:text-lg xm:text-xs">
+                    Total Units : {sched.total_units}
+                  </h1>
+                </div>
+              </>
+            ))}
+            <div className="h-[500px] overflow-auto grid-in-table">
+              {" "}
+              {/* Set horizontal overflow here */}
+              <GenerateTable data={data} />
             </div>
-            
           </div>
         </div>
       </div>
