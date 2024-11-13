dbabfb2c639eb7afec6342f8504f6d12bb66a0c7
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-11-13 20:32:40 +0800

    fixed building button not showing

--- client/src/components/SubAdmin/Management/Building/Files/BuildingRow.jsx
+++ client/src/components/SubAdmin/Management/Building/Files/BuildingRow.jsx
@@ -1,6 +1,7 @@
 const BuildingRow = ({ filteredBuildings, openUpdate, toggleDialog }) => {
 
-  const hasData = filteredBuildings.name && filteredBuildings.available_rooms;
+  const hasData = filteredBuildings.name;
+  console.log(hasData)
 
   return (
     <tr className="h-16">
