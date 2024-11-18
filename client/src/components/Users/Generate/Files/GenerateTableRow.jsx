a1259a7a61a69a3a6e4b058a27b99d8c99b423ec
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-11-19 00:19:37 +0800

    added borders for schedule

--- client/src/components/Users/Generate/Files/GenerateTableRow.jsx
+++ client/src/components/Users/Generate/Files/GenerateTableRow.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const GenerateTableRow = ({course, sched}) => {
   return (
     <tr>
-        <td>{course.course.code}</td>
-        <td>{course.course.description}</td>
-        <td>{course.course.lecture_units}</td>
-        <td>{course.course.lab_units}</td>
-        <td>{course.lecture_time_range.day_of_week} {course.course.lab_units === 1 ? "/ " + course.lab_time_range.day_of_week : ""}</td>
-        <td>{course.lecture_time_range.start_time} - {course.lecture_time_range.end_time}</td>
-        <td>{course.lecture_room.number} {course.course.lab_units > 0 && !course.lecture_room.number ? course.lab_room.number : ""} </td>
-        <td>{sched.section_label}</td> 
+        <td className='border border-collapse border-gray-300'>{course.course.code}</td>
+        <td  className='border border-collapse border-gray-300'>{course.course.description}</td>
+        <td  className='border border-collapse border-gray-300'>{course.course.lecture_units}</td>
+        <td  className='border border-collapse border-gray-300'>{course.course.lab_units}</td>
+        <td  className='border border-collapse border-gray-300'>{course.lecture_time_range.day_of_week} {course.course.lab_units === 1 ? "/ " + course.lab_time_range.day_of_week : ""}</td>
+        <td  className='border border-collapse border-gray-300'>{course.lecture_time_range.start_time} - {course.lecture_time_range.end_time}</td>
+        <td  className='border border-collapse border-gray-300'>{course.lecture_room.number} {course.course.lab_units > 0 && !course.lecture_room.number ? course.lab_room.number : ""} </td>
+        <td  className='border border-collapse border-gray-300'>{sched.section_label}</td> 
     </tr>
   )
 }
